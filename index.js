4b603d73acfccdb5b85aaac54a34993ea280168e
Petr Janik <peta.janik@email.cz>
2021-08-31 11:54:54 +0200

    Edit server listening message

--- index.js
+++ index.js
@@ -37,5 +37,5 @@ io.on('connection', (socket) => {
 const PORT = process.env.PORT || 3002;
 
 server.listen(PORT, () => {
-    console.log(`listening on *:${PORT}`);
+    console.log(`listening on PORT: ${PORT}`);
 });
